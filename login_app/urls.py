003afeb88c4bddfa166d0068973796a53228dc9e
Ehsan-Fouladi <fooehan3@gmail.com>
2022-09-22 15:58:11 +0330

    update

--- login_app/urls.py
+++ login_app/urls.py
@@ -1,7 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
-
+app_name = 'article'
 urlpatterns = [
-    path("", views.forms22)
+    path("", views.forms22, name='article')
 ]
